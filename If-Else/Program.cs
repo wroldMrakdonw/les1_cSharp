ae7ab2711f2de90e9643f4625b832e2fd8351f27
wrold <tema_akim@mail.ru>
2022-08-28 17:06:45 +0400

    If-else

---
+++ If-Else/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+if (username == "Тёма DevCode:dv23fg") {
+    Console.WriteLine("Привет, разработчик");
+}
+else {
+    if (username == "Мама DevCode:dv23fg") {
+        Console.WriteLine("Когда будешь учиться?");
+    }
+    else {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
+}
