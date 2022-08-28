f5ef58600aa866d5847f7dbf6111401bcec6b9d1
wrold <tema_akim@mail.ru>
2022-08-28 16:34:40 +0400

    Sum

---
+++ Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int numA = new Random().Next(1,11);
+Console.Write(numA);
+Console.Write("+");
+int numB = new Random().Next(1,11);
+Console.Write(numB);
+Console.Write("=");
+Console.WriteLine(numA + numB);
