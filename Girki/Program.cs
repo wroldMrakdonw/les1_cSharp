d33817f0ffee51b638f368750ec3e355a0d78984
wrold <tema_akim@mail.ru>
2022-08-28 18:11:01 +0400

    Girki

---
+++ Girki/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 54;
+int b = 5;
+int c = 439;
+int d = -294;
+int e = 0;
+
+int max = a;
+
+if (max < b) {
+    max = b;
+}
+
+if (max < c) {
+    max = c;
+}
+
+if (max < d) {
+    max = d;
+}
+
+if (max < e) {
+    max = e;
+}
+
+Console.WriteLine(max);
